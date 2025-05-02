0179eab78c8c335974143633919c178426e3e6f5
paulimasser <paulina.masse@gmail.com>
2025-05-01 23:40:52 -0400

    Scripts actualizados

--- scripts/alignment.sh
+++ scripts/alignment.sh
@@ -4,7 +4,18 @@
 INPUT="/home/paulinamasser/bioinformatics/phylogenics-project/data/raw/clean_sequences.fasta"
 ALIGNED_DIR="/home/paulinamasser/bioinformatics/phylogenics-project/data/aligned"
 
-mafft  --localpair --maxiterate 1000 --thread 8 $INPUT > $ALIGNED_DIR/aligned_sequences.fasta
+mafft \
+	--localpair \
+	--maxiterate 1000 \
+	--adjustdirection \
+	--leavegappyregion \
+	--op 3 \
+	--ep 1 \
+	 $INPUT > $ALIGNED_DIR/aligned_sequences.fasta
+
+# Verificacion
+echo "Longitud alineamiento: $(bioawk -c fastx '{print length($seq); exit}' $ALIGNED_DIR/aligned_sequences.fasta) bp"
+
 
 echo "Alineamiento completado. Resultado en $ALIGNED_DIR/aligned-sequences.fasta"
 
--- scripts/combine_sequence.sh
+++ scripts/combine_sequence.sh
@@ -5,6 +5,13 @@ RAW_DIR="/home/paulinamasser/bioinformatics/phylogenics-project/data/raw"
 OUTPUT_DIR="/home/paulinamasser/bioinformatics/phylogenics-project/data/raw"
 
 # Combinar las secuencias
-cat $RAW_DIR/*.fasta > $OUTPUT_DIR/combined_sequences.fasta
+cat $RAW_DIR/*.fasta |
+tr - d '\r' | 
+awk '{
+	gsub(/%s/, "");
+	if (/^>/ {if (NR>1) print ""; print}
+	else {print "%s", $0}
+} END {print ""}' > $OUTPUT_DIR/combined_clean_sequences.fasta
 
-echo "Secuencias combinadas en $OUTPUT/combined_sequences.fasta"
+echo "Secuencias combinadas: $(grep -c "^>" $OUTPUT/combined_clean_sequences.fasta"
+echo "Longitud promedio: $(bioawk -c fastx '{print length($seq)} $OUTPUT/combined_clean_sequences.fasta | awk '{sum+=$1} END {print sum/NR}')"
